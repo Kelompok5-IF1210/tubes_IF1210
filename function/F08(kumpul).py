11ecea8fe59c3de2b83c22691f7dcddde1495815
Intan45 <intanuraini1212@gmail.com>
2023-04-23 14:24:12 +0700

    Update F08(kumpul).py

--- function/F08(kumpul).py
+++ function/F08(kumpul).py
@@ -11,21 +11,19 @@ def csv_to_array(filecsv):
     arraybaru = []
     with open(filecsv, 'r') as file:
         dlmtandapetik = False   # (cek dia di dalam "" or no)
-        cell_data = '' # simpen data untuk csv yang lg di proses. 
-        # untuk kumpulin karakter dari file sampe koma atau smpe ktmu jeda baris.
-        row_data = [] # nyimpen data untuk baris yg lg di proses. 
-        # kalo ad jeda baris, ditambahin ke daftar array_csv
+        cell_data = '' 
+        row_data = [] 
         for char in file.read():
-            if char == ',' and not dlmtandapetik:
+            if char == ';' and not dlmtandapetik:
                 if cell_data != '':
                     row_data += [cell_data]
-                cell_data = ''
+                cell_data =""
             elif char == '\n' and not dlmtandapetik:
                 if cell_data != '':
                     row_data += [cell_data]
                 arraybaru += [row_data]
                 row_data = []
-                cell_data = ''
+                cell_data =""
             elif char == '"':
                 dlmtandapetik = not dlmtandapetik
             else:
@@ -36,30 +34,26 @@ def csv_to_array(filecsv):
             arraybaru += [row_data]
     return arraybaru
 
-array = csv_to_array("usnm.csv")
+def batch_kumpul():
+    jin_data = csv_to_array("usnm.csv")
+    jumlah_jin = lenn(jin_data)
 
-# ALGORITMA UTAMA
-file = open("usnm.csv", 'r')
-jumlah_data = lenn(list(file))
+    # Cek jumlah Jin Pengumpul
+    count = 0
+    for i in range (jumlah_jin):
+        if jin_data[i][2] == "jin pengumpul":
+            count += 1
 
-# Cek jumlah Jin Pengumpul
-count = 0
-for i in range (jumlah_data):
-    if "jin_pengumpul" in array[i]:
-        count += 1
+    if count == 0:
+        print("Kumpul gagal. Anda tidak punya jin pengumpul. Silahkan summon terlebih dahulu")
 
-if count == 0:
-    print("Kumpul gagal. Anda tidak punya jin pengumpul. Silahkan summon terlebih dahulu")
+    else:
+        bahan_bangunan = csv_to_array("bahan_bangunan.csv")
 
-else:
-    bahan_bangunan = csv_to_array("bahan_bangunan.csv")
-    pasir = bahan_bangunan [0][2]
-    batu = bahan_bangunan [1][2]
-    air = bahan_bangunan [2][2]
+        for i in range (count):
+            bahan_bangunan[0][2] = int(bahan_bangunan[0][2]) - random.randint(1,5)
+            bahan_bangunan[1][2] = int(bahan_bangunan[1][2]) - random.randint(1,5)
+            bahan_bangunan[2][2] = int(bahan_bangunan[2][2]) - random.randint(1,5)
 
-    for i in range (count):
-            pasir += random.randint(1,5)
-            batu += random.randint(1,5)
-            air += random.randint(1,5)
+        print(f"Mengerahkan {count} jin untuk mengumpulkan bahan. Jin menemukan total {bahan_bangunan[0][2]} pasir, {bahan_bangunan[1][2]} batu, dan {bahan_bangunan[2][2]} air.")
 
-    print(f"Mengerahkan {count} jin untuk mengumpulkan bahan. Jin menemukan total {pasir} pasir, {batu} batu, dan {air} air.")
