4b54d1e2384eb11e7ad47ca0b962ac0a10837a7a
denise <117981712+denoseu@users.noreply.github.com>
2023-04-30 15:51:48 +0700

    Update F05.py

--- function/F05.py
+++ function/F05.py
@@ -1,102 +1,40 @@
+from Type import effective as eff
 
-# FUNGSI BUATAN
-# len
-def lenn(str):
-    count = 0
-    for i in str:
-        count += 1
-    return count
-# append
-def appendx(listlama, ygmauditambah):
-    listbaru = [None] * (lenn(listlama) + 1)
-
-    for i in range(lenn(listlama)):
-        listbaru[i] = listlama[i]
-
-    listbaru[lenn(listlama)] = ygmauditambah
-
-    return listbaru
-# split
-def splitx(str, mark):
-    listbaru = []
-    temp = 0
-
-    for i in range(lenn(str)):
-        if str[i] == mark:
-            listbaru = appendx(listbaru,(str[temp:i]))
-            temp = i + 1
-
-    listbaru = appendx(listbaru,(str[temp:]))
-
-    return listbaru
-
-
-fileBukaan = open('user.csv','r')
-
-# PARSER BUATAN (ARRAY)
-# divider untuk memisah per word
-finArry = []
-
-for line in fileBukaan:
-
-    tempStr = ""
+def ubahjin(role:str, user:eff) -> eff:
+    if role!="bandung_bondowoso":
+        print("Kamu tidak memiliki akses ke fitur ini!")
+    else:
+        nama = str(input("Masukkan username jin: "))  
     
-    for i in line:
-
-        if i == ";" or i == "\n":
-            finArry = appendx (finArry,tempStr)
-            tempStr = ""
-
-        else:
-            tempStr += i
-
-# MAIN DEF
-def ubahJin(role,nama,newArry):
-    if role == "bandung_bondowoso":
-            
         x = False
-        
         # JIN SEARCHER
-        for i in range(9,len(newArry),3):
-            if nama == newArry[i]:
+        for i in range (2, user.NEff):
+            if nama == user.mtx[i][0]:
                 x = True
+    
         if x == True:
-            for i in range (9,len(newArry),3):
-                if nama == newArry[i]:
-                    
+            for i in range (2,user.NEff):
+                if nama == user.mtx[i][0]:
                     # ROLE CHANGE
                     inputSalah = True
                     while inputSalah: 
-                        
                         confirm = input("Apakah anda ingin mengubah role jin terserbut? (y/n): ")
+                        
                         if confirm == "y":
                             inputSalah = False
-                            if newArry[i+2] == "Pembangun":
-                                newArry[i+2] = "Pengumpul"
-                            elif newArry[i+2] == "Pengumpul" :
-                                newArry[i+2] = "Pembangun"
-
+                            if user.mtx[i][2] == "jin_pembangun":
+                                user.mtx[i][2] = "jin_pengumpul"
+                            elif user.mtx[i][2] == "jin_pengumpul" :
+                                user.mtx[i][2] = "jin_pembangun"
+                            print("Jin berhasil diubah.")
+                        
+                        # APABILA USER MENGDECLINE
                         elif confirm == "n":
                             inputSalah = False
-                    
-    # APABILA USER MENGDECLINE
-        else:
+                            print("Jin tidak jadi diubah.")
+
+        else: # x==False
             print("Tidak ada jin dengan username tersebut.")
-        # OUTPUT
-        return newArry
-    else:
-        return newArry
 
-# yg dipanggil di main 
-def f05(arrayUser):
-    nama = str(input("Masukkan username jin : "))
-    finArry = ubahJin(nama,arrayUser)
-    
-    return finArry
-
-      
-            
-        
-          
-            
-
+    # OUTPUT
+    return user
