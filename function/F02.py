dca7a347ef1c68bd6c179d668f82b6b3ecd5ed51
denise <117981712+denoseu@users.noreply.github.com>
2023-04-30 10:35:29 +0700

    Update and rename F02 Logout.py to F02.py

---
+++ function/F02.py
@@ -0,0 +1,11 @@
+def logout(username:str, role:str, isLoggedIn:bool) -> tuple[str,str,bool]:
+    if (isLoggedIn==True):
+        isLoggedIn = False
+        username=""
+        role=""
+        print("Berhasil logout")
+    else:
+        print("Logout gagal!")
+        print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+    
+    return (username, role, isLoggedIn)
