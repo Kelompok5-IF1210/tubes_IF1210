14100b7d83a26343678e03eeef3549929973a4f1
denise <117981712+denoseu@users.noreply.github.com>
2023-04-30 15:55:26 +0700

    Update and rename F11 Hancurkan Candi.py to F11.py

---
+++ function/F11.py
@@ -0,0 +1,32 @@
+from Type import effective as eff
+from commands import del_mtx
+
+def hancurkancandi(role:str, candi:eff) -> eff:
+    if role!="roro_jonggrang":
+        print("Kamu tidak memiliki akses fitur ini!")
+    else:
+        id = int(input("Masukkan ID Candi: "))
+
+        Cekcandi = False
+        Cekpanjang = candi.NEff
+
+        for i in range (Cekpanjang):
+            if candi.mtx[i][0] == str(id):
+                Cekcandi = True
+                break
+        
+        if Cekcandi == False:
+            print("Tidak ada candi dengan ID tersebut")
+        else:
+            Se7 = input(f"Apakah anda yakin ingin menghancurkan candi ID: {id} (Y/N)? ")
+            if Se7 == "Y":
+                for i in range (candi.NEff): 
+                    if candi.mtx[i][0] == id:
+                        candi.mtx = del_mtx(candi.mtx,i,candi.NEff) # geser candi
+                        candi.NEff-=1 # update NEff candi
+                print("Candi telah berhasil dihancurkan.")
+
+            else:
+                print("Candi tidak jadi dihancurkan.")
+
+    return candi
